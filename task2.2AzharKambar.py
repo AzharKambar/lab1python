1f2376a633e48df43d98381fc9267ebbf48c27f6
AzharKambar <124907708+AzharKambar@users.noreply.github.com>
2023-09-26 15:59:37 +0300

    Update task2.2AzharKambar.py

--- task2.2AzharKambar.py
+++ task2.2AzharKambar.py
@@ -1,1 +1,7 @@
-
+#вводим число
+num=input("Enter a four-digit number:")
+#выводим на экран позиции четырехзначного числа
+print("The digit in the thousands position is",  num[0])
+print("The number in the hundreds position is", num[1])
+print("The digit in the tens position is", num[2])
+print("The digit in the tens position is", num[3])
