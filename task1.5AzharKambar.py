ab82bbaf63be11a820b479b0321a07362f47f420
AzharKambar <124907708+AzharKambar@users.noreply.github.com>
2023-09-27 05:20:37 +0300

    task1.5AzharKambar.py

--- task1.5AzharKambar.py
+++ task1.5AzharKambar.py
@@ -1,8 +1,12 @@
+#код в котором может возникнуть исключение помещено в try
+try:
 #ввод длины ребра
-edgelength=int(input('Введите длину ребра куба'))
+edge_length=int(input('Введите длину ребра куба: '))
 #рассчет объема куба
-volume=edgelength**3
+volume=edge_length**3
 #рассчет площади поверхности куба
-area=6*edgelength**2
+area=6*edge_length**2
 #вывод на экран значения объема и площади с использованием f-строки
 print(f"Volume= {volume} \n Total surface area = {area}")
+except ValueError:
+print("Ввод данных неверный")
