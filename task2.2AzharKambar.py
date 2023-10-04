987f075e031df8c7586d08a1cd85d4fcf67d5d1a
AzharKambar <124907708+AzharKambar@users.noreply.github.com>
2023-10-04 06:26:41 +0300

    Update task2.2AzharKambar.py

--- task2.2AzharKambar.py
+++ task2.2AzharKambar.py
@@ -1,11 +1,11 @@
 #код в котором может возникнуть исключение помещено в try
 try:
-#вводим четырехзначное число
-num=input("Enter a four-digit number:")
-#выводим на экран позиции четырехзначного числа с помощью индексов
-print("The digit in the thousands position is",  num[0])
-print("The number in the hundreds position is", num[1])
-print("The digit in the tens position is", num[2])
-print("The digit in the tens position is", num[3])
+    #вводим четырехзначное число
+    num=input("Enter a four-digit number:")
+    #выводим на экран позиции четырехзначного числа с помощью индексов
+   first_position=num/10
+   second_position=num%10
+   print(f"Позиция первая {first})
+
 except ValueError:
-print("Ввод данных неверный")
+    print("Ввод данных неверный")
